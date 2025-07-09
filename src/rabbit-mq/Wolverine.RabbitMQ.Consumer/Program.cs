5870318c07581da7ed28b20d2f00ad106e7a2929
Lenerson Velho Nunes <lenerson.nunes@lepton.tech>
2025-07-09 09:14:19 -0300

    feat: Creating the Wolverine example.

---
+++ src/rabbit-mq/Wolverine.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Wolverine;
+using Wolverine.RabbitMQ;
+
+using var host = Host.CreateDefaultBuilder()
+    .UseWolverine(options =>
+    {
+        options.UseRabbitMq(c =>
+        {
+            c.HostName = "localhost";
+        }).AutoProvision();
+
+        options.ListenToRabbitQueue("my-message-queue");
+    }).Build();
+
+await host.RunAsync();
